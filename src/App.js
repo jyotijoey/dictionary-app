c8caf4f7e4ebddb526a7305ba97847b6b6f01a1b
jyotijoey <jburnwal5@gmail.com>
2021-06-15 11:42:40 +0530

    Container component file created and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Container from "./Container";
 
 function App() {
   return (
     // using BEM
     <div className="app"> 
       <h1>THe app component</h1>
+      <Container />
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Container() {
+    return (
+        <div className="container">
+            <h1>Container component</h1>
+        </div>
+    )
+}
+
+export default Container;
