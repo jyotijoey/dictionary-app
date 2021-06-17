0064bcf5c935f7bc19324952c7712043c19b88be
jyotijoey <jburnwal5@gmail.com>
2021-06-17 17:20:26 +0530

    debugged form

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 import "./SearchBar.css";
 import SearchIcon from '@material-ui/icons/Search';
+import axios from 'axios';
+
 
 function SearchBar() {
-    const [input, setInput] = useState({});
+    const [input, setInput] = useState({
+        term:"",
+    });
+    function Submit(e){
+        e.preventDefault();
+        axios.post("http://localhost:4000/search",{
+            term: input.term
+        })
+        .then((res)=>{
+            console.log(res.data);
+        })
+    }
+
+    function Handle(e){
+        const newInput={...input}
+        newInput[e.target.name]= e.target.value
+        setInput(newInput)
+        console.log(input);
+    }
+
     return (
         
         <form 
-        action="http://localhost:4000/search" 
+        // action="http://localhost:4000/search" 
         method="post" 
-        onSubmit={(e)=> e.preventDefault()}
+        onSubmit={(e)=> Submit(e)}
         className="searchBar">
             <input 
                 type="text"
-                onChange={(e)=> setInput(e.target.value)}  
+                onChange={(e)=> Handle(e)}  
                 name="term"
             /> 
             <button onSubmit={()=> input} type="submit"><SearchIcon className="searchBar__icon"/></button>
