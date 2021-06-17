cb18f4cf9916c31bbf9d76c55f3a6ba1da6261f4
jyotijoey <jburnwal5@gmail.com>
2021-06-18 01:57:06 +0530

    css fixed in listitem.js

--- src/ListItem.js
+++ src/ListItem.js
@@ -2,41 +2,72 @@
 import React, { useState } from "react";
 import "./ListItem.css";
 import Modal from "react-modal";
-import CloseIcon from '@material-ui/icons/Close';
+import CloseIcon from "@material-ui/icons/Close";
 
 function ListItem({ item }) {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState({})
+  const [value, setValue] = useState({});
 
-  function HandleClick(_id, term, definition, phrase) {
-    setValue({_id, term, definition, phrase});
+  function HandleClick(
+    _id,
+    term,
+    definition,
+    phrase,
+    sDefinition,
+    spell,
+    phrase2
+  ) {
+    setValue({ _id, term, definition, phrase, sDefinition, spell, phrase2 });
     setOpen(true);
     console.log(value);
     console.log(value._id);
   }
   return (
     <>
-      {item.map(({ term, definition, phrase, _id, sDefinition }) => {
-        return (
-          <div>
-          <div
-            key={_id}
-            className="listItem"
-            onClick={() => HandleClick(_id, term, definition, phrase)}
-          >
-            <h1>{term.toUpperCase()}:</h1>
-            <p>{definition.substring(0, 30).toUpperCase()}...</p>
-          </div>
-          <Modal className="listItem__modal" isOpen={open}>
-            <h1>{value.term}
-            <button className="listItem__btn" onClick={()=>setOpen(false)}><CloseIcon /></button>
-            </h1>
-            
-            <p>{value.definition}</p>
-          </Modal>
-          </div>
-        );
-      })}
+      {item.map(
+        ({ term, definition, phrase, _id, sDefinition, spell, phrase2 }) => {
+          return (
+            <div>
+              <div
+                key={_id}
+                className="listItem"
+                onClick={() =>
+                  HandleClick(
+                    _id,
+                    term,
+                    definition,
+                    phrase,
+                    sDefinition,
+                    spell,
+                    phrase2
+                  )
+                }
+              >
+                <h1>{term.toUpperCase()}:</h1>
+                <p>{sDefinition?.substring(0, 30).toUpperCase()}...</p>
+              </div>
+
+              <Modal className="listItem__modal" isOpen={open}>
+              <button
+                    className="listItem__btn"
+                    onClick={() => setOpen(false)}
+                  >
+                    <CloseIcon />
+                  </button>
+                <h1>{value.term}</h1>
+                <h4>{value?.sDefinition}</h4>
+                <h4>Phonetic Spelling: {value?.spell}</h4>
+                <p>{value?.definition}</p>
+                <div className="listItem__phrases">
+                <h5>Used Cases</h5>
+                <li>{value?.phrase}</li>
+                <li>{value?.phrase2}</li>
+                </div>
+              </Modal>
+            </div>
+          );
+        }
+      )}
     </>
   );
 }
